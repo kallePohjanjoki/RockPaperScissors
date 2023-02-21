7c1ce3c0bf9bb391e7b4a05e97949664b93a0caf
Kalle Pohjanjoki <kalle@pohjanjoki.com>
2023-02-21 20:08:03 +0200

    Initial commit

---
+++ Rock Paper Scissors/Rock_Paper_ScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rock_Paper_ScissorsApp.swift
+//  Rock Paper Scissors
+//
+//  Created by Kalle Pohjanjoki on 21.2.2023.
+//
+
+import SwiftUI
+
+@main
+struct Rock_Paper_ScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
